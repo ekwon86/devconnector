0a59c6bdd71a42e5494657d1226b1761f95e93d9
Eugene Kwon <ekwon86@gmail.com>
2018-04-25 07:57:51 -0700

    finished register route successfully saving user to database and generating a hashed password

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin@ds163294.mlab.com:63294/ekwon-mern-app'
+  mongoURI: 'mongodb://localhost:27017/ekwon-mern-app'
+  // mongoURI: 'mongodb://admin:admin@ds163294.mlab.com:63294/ekwon-mern-app'
 };
