f89cf4940f74c767b2212e9099ba89282bfbe60f
Eugene Kwon <ekwon86@gmail.com>
2018-04-27 08:04:42 -0700

    working on profile creation route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,17 +13,46 @@ const User = require('../../models/User');
 // @desc    Get current user's profile page
 // @access  Private
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const errors = {};
+  const errors = {};
 
-    Profile.findOne({ user: req.user.id })
-      .then(profile => {
-        if(!profile) {
-          errors.noprofile = 'There is no profile for this user';
-          return res.status(404).json(errors);
-        }
-        res.json(profile);
-      })
-      .catch(err => res.status(404).json(err));
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
 });
+
+// @route   POST api/profile
+// @desc    Create or edit user profile
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Get fields
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if(req.body.handle) profileFields.handle = req.body.handle;
+  if(req.body.company) profileFields.company = req.body.company;
+  if(req.body.website) profileFields.website = req.body.website;
+  if(req.body.location) profileFields.location = req.body.location;
+  if(req.body.bio) profileFields.bio = req.body.bio;
+  if(req.body.status) profileFields.status = req.body.status;
+  if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+
+  // Skill - split into array
+  if(typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(',');
+  }
+  // Social
+  profileFields.social = {};
+  if(req.body.youtube) profilefields.social.youtube = req.body.youtube;
+  if(req.body.twitter) profilefields.social.twitter = req.body.twitter;
+  if(req.body.linkedin) profilefields.social.linkedin = req.body.linkedin;
+  if(req.body.facebook) profilefields.social.facebook = req.body.facebook;
+  if(req.body.instagram) profilefields.social.instagram = req.body.instagram;
+});
+
 
 module.exports = router;
