7d5e58a76a56536b9d024a3e8137cc45cb672ade
Eugene Kwon <ekwon86@gmail.com>
2018-05-14 12:38:10 -0700

    finished back end moving on to front end

---
+++ validation/post.js
@@ -0,0 +1,24 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  // If input is empty to begin with, initialize to be an empty string with ternary operator
+  data.text = !isEmpty(data.text) ? data.text: '';
+
+  // Check if post is between 10 and 300 characters
+  if(!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = 'Post must be between 10 and 300 characters';
+  }
+
+  // Check if text is empty
+  if(Validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
